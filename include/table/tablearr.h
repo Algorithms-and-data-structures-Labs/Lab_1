8d15110f5e58ea226fe630c4e8c7719dc74c3d0b
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2024-03-06 23:05:47 +0300

    Arr

---
+++ include/table/tablearr.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+#include <locale>
+#include <string>
+
+#include <table/tablerec.h>
+
+template <typename TKey, typename TValue>
+class ArrayTable : public Table<TKey, TValue> {
+ private:
+  size_t currentIndex;
+
+ public:
+  ArrayTable() : currentIndex(0) {}
+
+  bool IsFull() const override {
+    if (count == TabMaxSize) return true;
+    else return false;
+  }
+
+  TValue* Find(TKey key) override {
+    for (auto& val : data)
+      if (val.key == key) return val.value;
+    return nullptr;
+  }
+
+  void Insert(TKey key, TValue value) override {
+    TabRec tab = {key, &value};
+    data.push_back(tab);
+    this->count++;
+  }
+
+  void Delete(TKey key) override {
+    for (size_t i = 0; i < data.size(); i++)
+      if (data[i].key == key) {
+        data[i] = data[data.size() - 1];
+        data.pop_back();
+        return;
+      }
+  }
+
+  int Reset() override {
+    currentIndex = 0;
+    return currentIndex;
+  }
+
+  int IsTabEnded() const override { return currentIndex >= data.size(); }
+
+  int GoNext() override {
+    currentIndex++;
+    return currentIndex;
+  }
+
+  TKey GetKey() const override { return data[currentIndex].key; }
+
+  TValue GetValuePtr() const override { return *data[currentIndex].value; }
+};
---
+++ include/table/tablerec.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#include <locale>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define TabMaxSize 100
+template <typename TKey, typename TValue>
+class Unorderable {
+ protected:
+  struct TabRec {
+    TKey key;
+    TValue* value;
+  };
+  vector<TabRec> data{};
+ public:
+  Unorderable() = default;
+  size_t size() const noexcept { return data.size(); }
+  TValue& operator[](size_t pos) { return data[pos].value; }
+};
+
+template <typename TKey, typename TValue>
+class Table : public Unorderable<typename TKey, typename TValue> {
+ protected:
+  int count;
+ public:
+  int GetDataCount() const { return count; }
+  bool IsEmpty() const {
+    if (count == 0) return true;
+    else return false;
+  }
+  virtual bool IsFull() const = 0;
+  virtual TValue* Find(TKey key) = 0;
+  virtual void Insert(TKey key, TValue value) = 0; 
+  virtual void Delete(TKey key) = 0;
+  virtual int Reset(void) = 0;
+  virtual int IsTabEnded(void) const = 0;
+  virtual int GoNext(void) = 0;
+  virtual TKey GetKey(void) const = 0;
+  virtual TValue GetValuePtr(void) const = 0;
+  friend ostream& operator<<(ostream& os, Table& tab) {
+    cout << "Table printing" << endl;
+    for (tab.Reset(); !tab.IsTabEnded(); tab.GoNext()) {
+      os << " Key: " << tab.GetKey() << " Val: " << *tab.GetValuePtr() << endl;
+    }
+    return os;
+  }
+};
